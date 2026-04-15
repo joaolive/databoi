d03aa09d64b0904f9540a2543fe7f9ce67203c2f
joaolive <devjoaolive@outlook.com>
2026-04-15 09:47:10 -0300

    chore: initial project setup

---
+++ src/main/java/com/joaolive/databoi/DataboiApplication.java
@@ -0,0 +1,13 @@
+package com.joaolive.databoi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataboiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataboiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joaolive/databoi/DataboiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaolive.databoi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataboiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
